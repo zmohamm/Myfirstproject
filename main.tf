eb19c6eafa5d0bcab2de57e3b2999c611e993fe6
zmohamm <44280632+zmohamm@users.noreply.github.com>
2022-08-07 03:55:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "vm_instances" {
+
+  
+    name         = var.name
+    #name         =  "vminstances"
+    project      =  var.project_id
+    machine_type =  var.machines_typ
+    zone         =  var.zone
+    #region       =  var.location
+
+    boot_disk {
+
+        initialize_params{
+
+            image = var.src_img
+            size =  var.disk_size_gb
+            type =  var.disk_type
+
+        }
+    }
+
+    tags = ["http-server"]
+
+    network_interface {
+          
+        network = "default"       // This enables private ip address
+        access_config {           //  This enables public ip address
+            
+        }
+    }
+  
+      
+        
+        
+
+}
