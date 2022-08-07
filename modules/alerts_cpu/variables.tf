f7702e8d09525ac90e99454a47794544a5333dea
zmohamm <44280632+zmohamm@users.noreply.github.com>
2022-08-07 17:22:57 +0530

    Create variables.tf

---
+++ modules/alerts_cpu/variables.tf
@@ -0,0 +1,15 @@
+variable "email_address" {
+    type =string
+    default = ""
+
+  
+}
+
+variable "project_id" {
+
+    type = string
+    default="expanded-origin-*****"
+        
+        
+  
+}
