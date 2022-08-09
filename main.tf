6e8055a6e49bc1964316eea35f7e92aecf7a17c2
zmohamm <44280632+zmohamm@users.noreply.github.com>
2022-08-09 15:26:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
+
+module "email_channel"{
+  source = "./modules/email_channel"
+  project_id = var.project_id
+
+
+}
+
+
+
 module "cpu_alert_policy" {
   source                  = "./modules/alerts_cpu"
 
   
   project_id              = var.project_id
+  notification_channels   = module.email_channel.email
   
 
 }
